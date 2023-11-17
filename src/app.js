c1bac82ecfeeb87af0f127d28baabd3f6893cd5f
cesarsan86 <86737129+cesarsan86@users.noreply.github.com>
2023-11-17 21:21:13 +0000

    los iconos no se ponen rojos,solucionar

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const num = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  const palos = ["♦", "♥", "♣", "♠"];
+
+  const randomNum = [Math.floor(Math.random() * num.length)];
+  const randomPalos = [Math.floor(Math.random() * palos.length)];
+
+  document.getElementById("numero").innerHTML = num[randomNum];
+
+  if (randomPalos === 0 || randomPalos === 1) {
+    document.getElementById(
+      "palosArriba"
+    ).innerHTML = `<span class="text-danger"> ${palos[randomPalos]} </span>`;
+
+    document.getElementById(
+      "palosAbajo"
+    ).innerHTML = `<span class="text-danger"> ${palos[randomPalos]} </span>`;
+  } else {
+    document.getElementById("palosArriba").innerHTML = palos[randomPalos];
+    document.getElementById("palosAbajo").innerHTML = palos[randomPalos];
+  }
+
+  //let button = document.getElementById("boton");
 };
