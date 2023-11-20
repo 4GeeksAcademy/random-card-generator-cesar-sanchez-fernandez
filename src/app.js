330c94206769a47f2edb12f8b1f04c3c38632efd
cesarsan86 <86737129+cesarsan86@users.noreply.github.com>
2023-11-20 21:00:32 +0000

    proyecto random-card terminado

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ window.onload = function() {
   const num = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
   const palos = ["♦", "♥", "♣", "♠"];
 
-  const randomNum = [Math.floor(Math.random() * num.length)];
-  const randomPalos = [Math.floor(Math.random() * palos.length)];
+  const randomNum = Math.floor(Math.random() * num.length);
+  const randomPalos = Math.floor(Math.random() * palos.length);
 
   document.getElementById("numero").innerHTML = num[randomNum];
 
@@ -19,7 +19,6 @@ window.onload = function() {
     document.getElementById(
       "palosArriba"
     ).innerHTML = `<span class="text-danger"> ${palos[randomPalos]} </span>`;
-
     document.getElementById(
       "palosAbajo"
     ).innerHTML = `<span class="text-danger"> ${palos[randomPalos]} </span>`;
@@ -28,5 +27,8 @@ window.onload = function() {
     document.getElementById("palosAbajo").innerHTML = palos[randomPalos];
   }
 
-  //let button = document.getElementById("boton");
+  let button = document.getElementById("boton");
+  button.addEventListener("click", _ => {
+    location.reload();
+  });
 };
